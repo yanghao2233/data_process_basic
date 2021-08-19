94a9a8c0249497b159937f65fe7e32d33214beab
yanghao2233 <89185981+yanghao2233@users.noreply.github.com>
2021-08-19 17:04:13 +0800

    提交 缺失值处理模块

---
+++ data_cleaning/missing_utils.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+# @Author: yanghao
+# @Create time: 2021/08/19 16:49
+# @Description:
+
+import pandas as pd
+
+
+class miss_value_pcs():
+    pass
+
+    def just_delete(self):
+        pass
+
+    def fill(self, type: str = 'front'):
+        pass
+
+    def bayes(self):
+        pass
+
+    def regress(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
