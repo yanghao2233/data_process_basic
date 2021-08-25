ad0ad58669e6b8a08f5d74976edd12a80d561408
YangHao2233 <jerry0102@foxmail.com>
2021-08-25 16:18:36 +0800

    新增 基础kmeans和基于自定义距离的kmeans

--- data_cleaning/clustering_utils.py
+++ data_cleaning/clustering_utils.py
@@ -6,26 +6,60 @@
 from sklearn.cluster import KMeans
 from sklearn import metrics
 from sklearn.mixture import GaussianMixture
+from pyclustering.cluster.kmeans import kmeans, kmeans_visualizer
+from pyclustering.utils.metric import distance_metric, type_metric
+from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
 import logging as logger
+import numpy as np
 
 
 class clustering():
-    def __init__(self, data, n_cluster):
+    def __init__(self, data: list or np.array, n_cluster: int):
         self.data = data
         self.n_cluster = n_cluster
 
-    def kmeans(self):
-        pass
+    def kmeans(self, max_iter: int = 300, n_init: int = 40, num_jobs: int = -1):
+        """
+        kmeans++ 聚类模型
+        :param
+            max_iter:
+            n_init:
+            num_jobs:
+        :return:
+            result:
+        """
+        kmeans_cluster = KMeans(n_clusters = self.n_cluster, max_iter = max_iter,
+                                n_init = n_init, init = 'k-means++', n_jobs = num_jobs)
+        result = kmeans_cluster.fit_predict(self.data)
+        return result
 
-    def kmeans_sd(self):
-        pass
+    def kmeans_sd(self, distance_function, if_visualize: bool = False):
+        """
+        基于自定义距离的 kmeans 聚类模型
+        :param
+            distance_function:
+            if_visualize:
+        :return:
+            clusters:
+        """
+        initial_centers = kmeans_plusplus_initializer(data = self.data, amount_centers = self.n_cluster).initialize()
+        km_metric = distance_metric(type_metric.USER_DEFINED, func = distance_function)
+        kmeans_instance = kmeans(self.data, initial_centers, metric = km_metric)
+        kmeans_instance.process()
+        clusters = kmeans_instance.get_clusters()
+        final_centers = kmeans_instance.get_centers()
+        if if_visualize:
+            kmeans_visualizer.show_clusters(self.data, clusters, final_centers)
+        return clusters
 
     def GMM(self, covar_type: str = 'full', to_list: bool = True):
         """
         高斯混合聚类模型
-        :param covar_type:
-        :param to_list:
+        :param
+            covar_type:
+            to_list:
         :return:
+            result:
         """
         gmm = GaussianMixture(n_components = self.n_cluster, covariance_type = covar_type, random_state = 2333)
         gmmModel = gmm.fit(self.data)
